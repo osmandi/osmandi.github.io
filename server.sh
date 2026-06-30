d881f8a0561841fe154e7c8d945cbb5d914977ab
Osmandi <osmandigt@gmail.com>
2026-06-30 06:36:18 -0500

    Fix server.sh script

--- server.sh
+++ server.sh
@@ -4,8 +4,9 @@
 podman build -t quarto -f Containerfile .
 
 # Run container
-podman run -d \
+podman run \
+	-d \
 	-v ./website:/app/website \
 	-p 8080:8080 \
 	quarto \
-	quarto preview website --port 8080 --host 0.0.0.0
+	/usr/bin/bash -c "quarto preview website --port 8080 --host 0.0.0.0"
